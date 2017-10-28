9e207d7f6b3a219c9f1a34b067cb5dd9fe2abd03
Eduardo Villas Boas dos Santos <eduardovillasboassantos@gmail.com>
2017-10-28 16:21:17 -0200

    Tornando padrao a chamada
    Tornando padrão a chamada, para que possamos ter a mesma medida.

--- MensureSortMethods/src/main/java/br/uem/paa/sortMethods/HeapSort.java
+++ MensureSortMethods/src/main/java/br/uem/paa/sortMethods/HeapSort.java
@@ -4,47 +4,49 @@ import br.uem.paa.util.SortMethod;
 
 public class HeapSort implements SortMethod {
 
-    private void buildMaxHeap(int[] v) {
-        for (int i = v.length / 2 - 1; i >= 0; i--)
-            maxHeapify(v, i, v.length);
+	private void buildMaxHeap(int[] v) {
+		for (int i = v.length / 2 - 1; i >= 0; i--)
+			maxHeapify(v, i, v.length);
 
-    }
+	}
 
-     private void maxHeapify(int[] vetor, int pos, int tamanhoDoVetor)  
-     {  
+	private void maxHeapify(int[] vetor, int pos, int tamanhoDoVetor) {
 
-         int max = 2 * pos + 1, right = max + 1;  
-         if (max < tamanhoDoVetor)  
-         {  
+		int max = 2 * pos + 1, right = max + 1;
+		if (max < tamanhoDoVetor) {
 
-             if (right < tamanhoDoVetor && vetor[max] < vetor[right])  
-                 max = right;
+			if (right < tamanhoDoVetor && vetor[max] < vetor[right])
+				max = right;
 
-             if (vetor[max] > vetor[pos])  
-             {  
-                 swap(vetor, max, pos);  
-                 maxHeapify(vetor, max, tamanhoDoVetor);  
-             }  
-         }  
-     }
+			if (vetor[max] > vetor[pos]) {
+				swap(vetor, max, pos);
+				maxHeapify(vetor, max, tamanhoDoVetor);
+			}
+		}
+	}
 
-    public void swap(int[] v, int j, int aposJ) {
-        int aux = v[j];
-        v[j] = v[aposJ];
-        v[aposJ] = aux;
-    }
+	public void swap(int[] v, int j, int aposJ) {
+		int aux = v[j];
+		v[j] = v[aposJ];
+		v[aposJ] = aux;
+	}
 
 	@Override
 	public long run(int[] array) {
-        long time = System.currentTimeMillis();
-		buildMaxHeap(array);
-        int n = array.length;
-
-        for (int i = array.length - 1; i > 0; i--) {
-            swap(array, i, 0);
-            maxHeapify(array, 0, --n);
-        }
-        
+		long time = System.currentTimeMillis();
+		heap(array);
 		return System.currentTimeMillis() - time;
+	}
+
+	private void heap(int[] array) {
+
+		buildMaxHeap(array);
+		int n = array.length;
+
+		for (int i = array.length - 1; i > 0; i--) {
+			swap(array, i, 0);
+			maxHeapify(array, 0, --n);
+		}
+
 	}
 }
