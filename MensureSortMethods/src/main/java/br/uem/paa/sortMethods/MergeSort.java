71ef76a9ec868acd416658ef87c5eef443b01cac
Filipe Bittencourt <fwsb@outlook.com>
2017-10-29 17:33:43 -0200

    Inclusão do mergeSort

--- MensureSortMethods/src/main/java/br/uem/paa/sortMethods/MergeSort.java
+++ MensureSortMethods/src/main/java/br/uem/paa/sortMethods/MergeSort.java
@@ -4,12 +4,53 @@ import br.uem.paa.util.SortMethod;
 
 public class MergeSort implements SortMethod {
 
+    private int[] array;
+    private int[] helper;
+
     @Override
     public void run(int[] array) {
         mergeSortable(array);
     }
     
-    private void mergeSortable(int[] array) {
-        //TODO colar aqui implementação do MergeSortFilipado.
+    public void mergeSortable(int[] array) {
+        this.array = array;
+        int lenfth = array.length;
+        this.helper = new int[lenfth];
+        mergesort(0, lenfth - 1);
+    }
+
+    private void mergesort(int low, int high) {
+        if (low < high) {
+            int middle = low + (high - low) / 2;
+            mergesort(low, middle);
+            mergesort(middle + 1, high);
+            merge(low, middle, high);
+        }
+    }
+
+    private void merge(int low, int middle, int high) {
+
+        for (int i = low; i <= high; i++) {
+            helper[i] = array[i];
+        }
+
+        int i = low;
+        int j = middle + 1;
+        int k = low;
+        while (i <= middle && j <= high) {
+            if (helper[i] <= helper[j]) {
+                array[k] = helper[i];
+                i++;
+            } else {
+                array[k] = helper[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= middle) {
+            array[k] = helper[i];
+            k++;
+            i++;
+        }
     }
 }
